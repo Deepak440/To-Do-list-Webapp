c96cbb533d5979ae4c14d4bb9893a3c7623009c7
Deepak <deepakthakur2051@gmail.com>
2020-12-23 09:55:49 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+
+const express  = require("express");
+
+
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("view engine" , "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+var items = [ ];
+
+app.get("/" , function(req ,res)
+{
+    var d = new Date();
+    var option = {
+        weekday : "long",
+        day : "numeric",
+        month : "long"
+    };
+    var day = d.toLocaleDateString("en-US" , option);
+    
+    res.render("list" , {foo : day , newitem : items });
+    
+});
+
+app.post("/" , function(req , res)
+{
+    item = req.body.newItem;
+    items.push(item);
+   
+   res.redirect("/");
+})
+
+app.listen(3000 , function(){
+console.log("Server started at port 3000");
+});
